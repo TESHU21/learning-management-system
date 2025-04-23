3b15ad554a32409e51817afca977a38fe2414739
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-04-23 11:33:03 +0300

    add course data

---
+++ frontend/src/pages/courses/data.jsx
@@ -0,0 +1,34 @@
+import CloudComputingimage from "../../assets/course/Cloud Computing.svg"
+import SoftwareDevelopmentImage from "../../assets/course/Software.svg"
+import DataScienceImage from "../../assets/course/Software.svg"
+export const course=[
+    {
+    id:1,
+    image:SoftwareDevelopmentImage,
+    title:"Software Development",
+    description:"Unlock your potential with comprehensive training in modern software development",
+    rating:4,
+    price:350,
+
+},
+    {
+    id:1,
+    image:DataScienceImage,
+    title:"Data Science Mastery",
+    description:"Equip yourself with the skills to analyze, interpret, and leverage data.",
+    rating:4,
+    price:350,
+
+},
+    {
+    id:1,
+    image:CloudComputingimage,
+    title:"Cloud Computing Expertise",
+    description:"Gain hands-on experience in cloud  preparing you to  manage scalable..",
+    rating:4,
+    price:350,
+
+},
+
+
+]
