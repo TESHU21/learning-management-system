6cc450e3f08d02ec7d8b2ebc56e2ec0d47fbb348
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-05-19 18:55:50 +0300

    Fix Sign Up Components

--- frontend/src/pages/registration/SignUp.jsx
+++ frontend/src/pages/registration/SignUp.jsx
@@ -7,25 +7,36 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@/contexts/authContext';
 import axiosInstance from '@/lib/axiosInstance';
 import GoogleLoginButton from '@/components/GoogleLoginButton';
+import { toast } from 'sonner';
 
 
 const SignUp = () => {
+   const [isLoading,setIsLoading]=useState(false)
+    const [successMessage,setSuccessMessage]=useState("")
+    const [errorMessage,setErrorMessage]=useState("")
     const navigate=useNavigate()
      const { signup } = useAuth();
 
 
-    const [isLoading,setIsLoading]=useState(false)
-    const [errorMessage, setErrorMessage] = useState("");
+   
     const handleSignUp=async(data)=>{
       try{
+        setIsLoading(true)
         
         const response=await signup(data)
         navigate("/verifyemail")
+        setSuccessMessage("Learner Registered Sucessfully")
+        toast.success("Learner Registered Sucessfully")
         
 
       }
       catch(error){
-        console.log(error.response.data)
+        console.log(error.response.data.errors[0].message)
+        setErrorMessage(error.response.data.errors[0].message)
+        toast.error("Faild To Register! try again")
+      }
+      finally{
+        setIsLoading(false)
       }
       
     }
@@ -43,7 +54,8 @@ const SignUp = () => {
         <span className='h-6 text-center'>or</span>
            
 
-        <FormComp schema={SignUpSchema} fields={fields} initialValues={initialValues} submitBtnText={"Sign Up"} onSubmit={handleSignUp}  />
+        <FormComp schema={SignUpSchema} fields={fields} initialValues={initialValues} submitBtnText={"Sign Up"}
+         onSubmit={handleSignUp} successMessage={successMessage}  errorMessage={errorMessage} isLoading={isLoading}/>
         <p className=' text-center underline decoration-blue-primary'>Already have an account ? <span className='text-blue-primary cursor-pointer' onClick={()=>navigate("/login")}>log in</span></p>
 
         </div>
