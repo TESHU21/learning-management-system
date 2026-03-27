8ed6f6e26415989bbe6d558446a6d360c4d144d9
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-03-27 23:24:24 +0300

    apply lazy loading

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,44 +1,52 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from '@/layout/Layout';
-import Home from '@/pages/home/Home';
-import Courses from '@/pages/courses/Courses';
-import CourseDetail from '@/pages/courses/components/CourseDetail';
-import Login from '@/pages/login/Login';
-import SignUp from '@/pages/registration/SignUp';
-import ForgotPassword from '@/pages/login/forgotpassword/ForgotPassword';
-import  OtpVerification  from '@/pages/login/forgotpassword/EmailVerification';
-import ResetPassword from '@/pages/login/forgotpassword/ResetPassword';
-import Checkout from '@/pages/checkout/Checkout';
-import Dashboard from '@/pages/portal/dashboard/Dashboard';
-import Settings from '@/pages/portal/settings/Settings';
-import Portal from '@/pages/portal/Portal';
-import VerifyEmail from '@/pages/registration/VerifyEmail';
-import EmailVerification from '@/pages/login/forgotpassword/EmailVerification';
-import ResetPasswordFlow from '@/pages/login/forgotpassword/ResetPasswordFlow';
-import PaymentSuccess from '@/pages/checkout/PaymentSuccess';
+import React, { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "@/layout/Layout";
+
+const Home = lazy(() => import("@/pages/home/Home"));
+const Courses = lazy(() => import("@/pages/courses/Courses"));
+const CourseDetail = lazy(
+  () => import("@/pages/courses/components/CourseDetail"),
+);
+const Login = lazy(() => import("@/pages/login/Login"));
+const SignUp = lazy(() => import("@/pages/registration/SignUp"));
+const ForgotPassword = lazy(
+  () => import("@/pages/login/forgotpassword/ForgotPassword"),
+);
+const Checkout = lazy(() => import("@/pages/checkout/Checkout"));
+const Dashboard = lazy(() => import("@/pages/portal/dashboard/Dashboard"));
+const Settings = lazy(() => import("@/pages/portal/settings/Settings"));
+const Portal = lazy(() => import("@/pages/portal/Portal"));
+const VerifyEmail = lazy(() => import("@/pages/registration/VerifyEmail"));
+const EmailVerification = lazy(
+  () => import("@/pages/login/forgotpassword/EmailVerification"),
+);
+const PaymentSuccess = lazy(() => import("@/pages/checkout/PaymentSuccess"));
+
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="courses" element={<Courses />} />
-        <Route path="courses/:id" element={<CourseDetail />} />
-        <Route path="login" element={<Login />} /> {/* <-- no slash here */}
-        <Route path='signup' element={<SignUp/>}/>       
-         <Route path="forgotpassword" element={<ForgotPassword />} />
+    <Suspense fallback={<div />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="courses/:id" element={<CourseDetail />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="forgotpassword" element={<ForgotPassword />} />
 
-        <Route path='checkout' element={<Checkout/>}/>
-        <Route path='dashboard' element={<Dashboard/>}/>
-        <Route path='settings' element={<Settings/>}/>
-        <Route path='portal' element={<Portal/>}/>
-        <Route path='verifyemail' element={<VerifyEmail/>}/>
-        <Route path="/payment-success" element={<PaymentSuccess/>}/>
-        <Route path="/reset-password/:token" element={<EmailVerification/>}/>
-
-      </Route>
-
-    </Routes>
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="portal" element={<Portal />} />
+          <Route path="verifyemail" element={<VerifyEmail />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />
+          <Route
+            path="/reset-password/:token"
+            element={<EmailVerification />}
+          />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
